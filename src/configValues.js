9f0397379bb7a3689569e606316a2f456559c3d0
Marc Littlemore <marc.littlemore@gmail.com>
2017-05-06 16:58:04 +0100

    Removed old code

--- src/configValues.js
+++ src/configValues.js
@@ -6,16 +6,4 @@ const getValueFromEnvironmentOrConfig = (environmentVariableKey, configKey) => {
         config.get(configKey);
 };
 
-// const APP_SECRET = () => getValueFromEnvironmentOrConfig('MESSENGER_APP_SECRET', 'appSecret');
-
-// // Generate a page access token for your page from the App Dashboard
-// const PAGE_ACCESS_TOKEN = () => getValueFromEnvironmentOrConfig('MESSENGER_PAGE_ACCESS_TOKEN', 'pageAccessToken');
-
-// // URL where the app is running (include protocol). Used to point to scripts and
-// // assets located at this address.
-// const SERVER_URL = () => getValueFromEnvironmentOrConfig('SERVER_URL', 'serverURL');
-
-// // Arbitrary value used to validate a webhook
-// const VALIDATION_TOKEN = () => getValueFromEnvironmentOrConfig('MESSENGER_VALIDATION_TOKEN', 'validationToken');
-
 export default getValueFromEnvironmentOrConfig;
